78edde6afd6018d8994fce67d1b8b2eab00284cf
David Ballesteros <david_ballesteros@outlook.com>
2019-10-17 20:37:30 +0800

    fix notif panel going nowhere

--- RPNRadio.UI/RPNRadio.UI.Android/MainActivity.cs
+++ RPNRadio.UI/RPNRadio.UI.Android/MainActivity.cs
@@ -21,6 +21,7 @@ using Acr.UserDialogs;
 namespace RPNRadio.UI.Droid
 {
     [Activity(Label = "RPN News & Radio", Icon = "@drawable/ic_launcher", Theme = "@style/MainTheme.Launcher", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [IntentFilter(new[] { Intent.ActionSend, Intent.ActionSendMultiple, Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable, Intent.CategoryAppMusic }, DataMimeTypes = new[] { "video/*", "audio/*" }, Label = "RPN News & Radio")]
     public class MainActivity : MvxFormsAppCompatActivity<Setup, Core.App, FormsApp>
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -38,7 +39,21 @@ namespace RPNRadio.UI.Droid
             CrossMediaManager.Current.Init(this);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
         }
-               
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            HandleIntent();
+        }
+
+        private async void HandleIntent()
+        {
+            if (await CrossMediaManager.Android.PlayFromIntent(Intent))
+            {
+                await Mvx.IoCProvider.Resolve<IMvxNavigationService>().Navigate<PlayerViewModel>();
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
